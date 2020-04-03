fe901b8eca88e94fe08b83619336ca7a85b55794
Homerocker <molodoy3561@gmail.com>
2020-04-04 00:56:57 +0300

    fixed typo that broke addon

--- RaidEventsVampiricBite/VampiricBite.lua
+++ RaidEventsVampiricBite/VampiricBite.lua
@@ -4,7 +4,7 @@ f:SetScript("OnEvent", function(_, _, ...)
   if arg[2] == "SPELL_CAST_SUCCESS" and table.contains({71728,71729,71476,71477}, arg[9]) then
     -- 71728 & 71476 10hc
     -- 71729 & 71477 25hc
-    RaidEvents:print(GetSpellLink(arg[9]..": "..UnitNameColored(arg[4]).."->"..UnitNameColored(arg[7])))
+    RaidEvents:print(GetSpellLink(arg[9])..": "..UnitNameColored(arg[4]).."->"..UnitNameColored(arg[7]))
   end
 end)
 f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
